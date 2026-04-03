1a88306969523d797159a2cb8212f66b25639c12
Chatkawin taola <flute.inrock@gmail.com>
2026-04-03 23:03:00 +0700

    t

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       data-theme="dark"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="h-full flex flex-col overflow-y-auto">{children}</body>
     </html>
   );
 }
